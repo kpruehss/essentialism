2274633c4983f26d85eb43ecf85406226e1d658f
Karsten Pruehss <Karsten@Pruehss.com>
2023-03-18 17:15:49 +1100

    [refac]: Add comments and increase readability of implementation

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/fizzbuzz.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/fizzbuzz.ts
@@ -1,16 +1,24 @@
+// Curry base function to check if a number is a multiple of another number
 const isMultiple =
-  (divisor: number) => (value: number) => value % divisor === 0;
+  (denominator: number) => (numerator: number) =>
+    numerator % denominator === 0;
 
+// Derive partially applied helpers for specific cases
 const isMultipleOf3 = isMultiple(3);
 const isMultipleOf5 = isMultiple(5);
 const isMultipleOf15 = isMultiple(15);
 
 export function fizzbuzz(...n: Array<number>): string {
   return n.reduce<string>((result, n) => {
+    // Validate Input
     if (n < 1 || n > 100) throw new Error("Out of range value detected");
+
+    // Check multiples and assemble result string
     if (isMultipleOf15(n)) return result += "FizzBuzz";
     if (isMultipleOf5(n)) return result += "Buzz";
     if (isMultipleOf3(n)) return result += "Fizz";
+
+    // Fallthrough case
     return result += n;
   }, "");
 }
