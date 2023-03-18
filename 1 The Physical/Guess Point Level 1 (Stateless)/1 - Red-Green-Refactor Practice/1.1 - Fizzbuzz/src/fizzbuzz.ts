86f8f3107e4efa57273474f7e61348b3e35c2cab
Karsten Pruehss <Karsten@Pruehss.com>
2023-03-18 16:52:17 +1100

    [impl]: aggregate inputs into array

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/fizzbuzz.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/fizzbuzz.ts
@@ -5,12 +5,12 @@ const isMultipleOf3 = isMultiple(3);
 const isMultipleOf5 = isMultiple(5);
 const isMultipleOf15 = isMultiple(15);
 
-export function fizzbuzz(n: number): string {
+export function fizzbuzz(...n: Array<number>): string {
   if (n < 1 || n > 100) throw new Error("Number outside allowable range (1-100)");
 
   if (isMultipleOf15(n)) return "FizzBuzz";
   if (isMultipleOf5(n)) return "Buzz";
   if (isMultipleOf3(n)) return "Fizz";
-  
+
   return String(n);
 }
