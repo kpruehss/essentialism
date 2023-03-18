6a25c665614305b612a9652a227a1d5fc35efd9c
Karsten Pruehss <Karsten@Pruehss.com>
2023-03-18 16:26:55 +1100

    [test]: Check number is in range

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/index.spec.ts
@@ -9,5 +9,8 @@ describe("fizzbuzz", () => {
     expect(typeof fizzbuzz(1)).toBe("string");
   });
 
+  it("should throw error if number is not between 1 and 100", () => {
+    expect(() => fizzbuzz(-1)).toThrow();
+  });
 
 });
