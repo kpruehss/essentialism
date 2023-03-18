cfde1356865f5a5458b9d9450268dff56807490f
Karsten Pruehss <Karsten@Pruehss.com>
2023-03-18 16:32:29 +1100

    [impl] return 'Buzz' for multiples of 5

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/fizzbuzz.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/fizzbuzz.ts
@@ -2,5 +2,6 @@ export function fizzbuzz(n: number): string {
   if (n < 1 || n > 100) throw new Error("Number outside allowable range (1-100)");
 
   if (n % 3 === 0) return "Fizz";
+  if (n % 5 === 0) return "Buzz";
   return String(n);
 }
