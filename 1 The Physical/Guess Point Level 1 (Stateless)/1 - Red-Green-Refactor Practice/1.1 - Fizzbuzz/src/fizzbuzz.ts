c6905ce81f0f8d7629f03ee8ae08098650f61b26
Karsten Pruehss <Karsten@Pruehss.com>
2023-03-18 16:42:25 +1100

    [impl] return "FizzBuzz" for multiple of 15

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/fizzbuzz.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/fizzbuzz.ts
@@ -3,11 +3,14 @@ const isMultiple =
 
 const isMultipleOf3 = isMultiple(3);
 const isMultipleOf5 = isMultiple(5);
+const isMultipleOf15 = isMultiple(15);
 
 export function fizzbuzz(n: number): string {
   if (n < 1 || n > 100) throw new Error("Number outside allowable range (1-100)");
 
-  if (isMultipleOf3(n)) return "Fizz";
+  if (isMultipleOf15(n)) return "FizzBuzz";
   if (isMultipleOf5(n)) return "Buzz";
+  if (isMultipleOf3(n)) return "Fizz";
+  
   return String(n);
 }
