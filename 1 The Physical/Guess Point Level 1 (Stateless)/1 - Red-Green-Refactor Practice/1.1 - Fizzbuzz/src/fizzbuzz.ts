93081c652a2cde3e00f0fa5c71fbe5dc842a5026
Karsten Pruehss <Karsten@Pruehss.com>
2023-03-18 16:38:55 +1100

    [refac] remove duplicate code & make more readable

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/fizzbuzz.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/fizzbuzz.ts
@@ -1,7 +1,13 @@
+const isMultiple =
+  (divisor: number) => (value: number) => value % divisor === 0;
+
+const isMultipleOf3 = isMultiple(3);
+const isMultipleOf5 = isMultiple(5);
+
 export function fizzbuzz(n: number): string {
   if (n < 1 || n > 100) throw new Error("Number outside allowable range (1-100)");
 
-  if (n % 3 === 0) return "Fizz";
-  if (n % 5 === 0) return "Buzz";
+  if (isMultipleOf3(n)) return "Fizz";
+  if (isMultipleOf5(n)) return "Buzz";
   return String(n);
 }
